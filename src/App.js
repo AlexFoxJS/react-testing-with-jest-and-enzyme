adca18414f4ebacae0986cbd5d6677e884122f1f
AlexFoxJS <alexfoxjs@gmail.com>
2019-01-02 21:29:53 +0300

    master: 15 Test Component Rendering
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App" data-test="component-app">
+				<h1>Hello World!</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
