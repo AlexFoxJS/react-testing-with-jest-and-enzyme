5549e7e43944513aa93ad39986cc174632f1f7ad
AlexFoxJS <alexfoxjs@gmail.com>
2019-01-04 00:13:29 +0300

    jotto: 36 Set up GuessedWords Component and Tests
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

---
+++ src/components/GuessedWords/GuessedWords.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const GuessedWords = () => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+GuessedWords.propTypes = {
+
+};
+
+export default GuessedWords;
