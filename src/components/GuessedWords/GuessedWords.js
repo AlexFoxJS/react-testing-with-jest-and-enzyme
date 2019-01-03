d0ed282c810760b902b002f7afd3140807b41f27
AlexFoxJS <alexfoxjs@gmail.com>
2019-01-04 01:02:03 +0300

    jotto: 42 Code for nonzero guessed words.mp4
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- src/components/GuessedWords/GuessedWords.js
+++ src/components/GuessedWords/GuessedWords.js
@@ -6,17 +6,39 @@ const GuessedWords = props => {
 
 	if (props.guessedWords.length === 0) {
 		contents = (
-			<span data-test="">
+			<span data-test="guess-instructions">
 				Try to guess the secret word!
 			</span>
+		)
+	} else {
+		const guessedWordsRows = props.guessedWords.map((word, index) => (
+			<tr data-test="guessed-word" key={index}>
+				<td>{word.guessedWord}</td>
+				<td>{word.letterMatchCount}</td>
+			</tr>
+		));
+
+		contents = (
+			<div data-test="guessed-words">
+				<h3>Guessed words</h3>
+				<table>
+					<thead>
+						<tr>
+							<th>Guess</th>
+							<th>Matching letter</th>
+						</tr>
+					</thead>
+					<tbody>
+						{ guessedWordsRows }
+					</tbody>
+				</table>
+			</div>
 		)
 	}
 
 	return (
 		<div data-test="component-guessed-word">
-			<span data-test="guess-instructions">
-				{contents}
-			</span>
+			{contents}
 		</div>
 	);
 
