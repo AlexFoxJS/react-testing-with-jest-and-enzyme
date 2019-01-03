71a2bf43d4dafa0140e8930363ed11fc438add2a
AlexFoxJS <alexfoxjs@gmail.com>
2019-01-04 01:15:59 +0300

    jotto: 44 Bootstrap Styling
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 	render() {
 
 		return (
-			<div className="App" data-test="component-app">
+			<div className="App container" data-test="component-app">
 				<h1>Jotto</h1>
 				<Congrats success={true} />
 				<GuessedWords guessedWords={[
