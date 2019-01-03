4901cbaed13a661f8c7eb3ce2f04a1241cbc9766
AlexFoxJS <alexfoxjs@gmail.com>
2019-01-03 22:59:48 +0300

    master: 27 Congrats Tests and the text Method
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Enzyme, {shallow} from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
-import Congrats from './Congrats';
+import { findByTestAttr } from '../../../test/testUtils';
 
-import { findByTestAttr } from 'test/testUtils'
+import Congrats from './Congrats';
 
 Enzyme.configure({adapter: new EnzymeAdapter});
 
