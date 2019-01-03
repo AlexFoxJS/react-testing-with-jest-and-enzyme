ddf2e3695767929d195a39f0e70ee7c71d7c981e
AlexFoxJS <alexfoxjs@gmail.com>
2019-01-04 01:31:24 +0300

    jotto: 46 Set up Redux
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - array of guessed words.
+ * @param {object} action - action to be reducer.
+ * @returns {boolean} - new success status.
+ */
+export default (state, action) => {
+	return false;
+}
